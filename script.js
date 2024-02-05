abd8819204c9a3e7579f83049c79af833048d31b
ajim9075shaikh <142928027+ajim9075shaikh@users.noreply.github.com>
2024-02-05 20:14:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  
+    if (!/^#([0-9A-Fa-f]{3})$/.test(shortHex)) {
+        throw new Error('Invalid short hex code format');
+	}
+    const [, r, g, b] = shortHex.match(/^#([0-9A-Fa-f])([0-9A-Fa-f])([0-9A-Fa-f])$/);
+
+    const fullHex = `#${r}${r}${g}${g}${b}${b}`;
+
+    return fullHex;
+}
+
+
+
+	
 };
 
 // Do not change the code below.
